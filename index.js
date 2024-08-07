815523c3b3f92196573bbcb7252361e770b2e599
gabrielgsv <gabriel.soares.vilela@gmail.com>
2024-08-07 20:56:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function formatNumber(number) {
+  return parseFloat(number.toFixed(2)).toLocaleString("pt-br");
+}
+
+function handleClick() {
+  const peso = +document.getElementById("peso").value;
+  const altura = +document.getElementById("altura").value / 100;
+  const resultado = document.getElementById("resultado");
+
+  const imc = peso / (altura * altura);
+
+  if (peso === 0 || altura === 0) {
+    resultado.textContent = "Por favor, preencha todos os campos.";
+  } else {
+    if (imc < 18.5) {
+      resultado.textContent = `Seu IMC é ${formatNumber(imc)}\n Magreza`;
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      resultado.textContent = `Seu IMC é ${formatNumber(imc)}\n Normal`;
+    } else if (imc >= 25 && imc <= 30) {
+      resultado.textContent = `Seu IMC é ${formatNumber(imc)}\n Sobrepeso`;
+    } else {
+      resultado.textContent = `Seu IMC é ${formatNumber(imc)}\n Obesidade`;
+    }
+  }
+}
